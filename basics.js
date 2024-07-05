012643ef1762806349d9749d641c539168b3a61e
geraz0 <gabbi.erazo1@gmail.com>
2024-07-05 10:57:35 -0400

    finished code and testes console ouput successfully

---
+++ basics.js
@@ -0,0 +1,28 @@
+let numbers1 = [1, 2, 3];
+
+let numbers2 = [4, 5, 6];
+
+let numbers = [...numbers1, ...numbers2];
+
+const square = (num) => num * num;
+
+const squares = numbers.map(num => num * num);
+// console.log(squares); // Output: [1, 4, 9, 16, 25]
+
+function isEven(num) {
+    return num % 2 === 0;
+  }
+// console.log(isEven(4));
+// console.log(isEven(7));
+
+//contains only the even squares from the squares array.
+const evenSquares = squares.filter(isEven);
+
+const firstEvenSquare = evenSquares[0];
+const secondEvenSquare = evenSquares[1];
+
+console.log(`Original numbers array: ${numbers}`);
+console.log(`Squares array: ${squares}`);
+console.log(`Even squares array: ${evenSquares}`);
+console.log(`First even square: ${firstEvenSquare}`);
+console.log(`Second even square: ${secondEvenSquare}`);
